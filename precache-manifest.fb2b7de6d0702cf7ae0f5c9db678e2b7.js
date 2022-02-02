d39872cf131fc08264400c95b26fab04723f2559
evelinastaniulyte <43463977+estaniulyte@users.noreply.github.com>
2022-02-02 09:38:33 +0200

    Updates

---
+++ precache-manifest.fb2b7de6d0702cf7ae0f5c9db678e2b7.js
@@ -0,0 +1,378 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea809cbe87e83aae78eff95fb872909",
+    "url": "/sourcery-academy-front-end/index.html"
+  },
+  {
+    "revision": "171d0af413857d522480",
+    "url": "/sourcery-academy-front-end/static/css/2.b0fee516.chunk.css"
+  },
+  {
+    "revision": "264ad4565f756c146f75",
+    "url": "/sourcery-academy-front-end/static/css/main.ea987205.chunk.css"
+  },
+  {
+    "revision": "171d0af413857d522480",
+    "url": "/sourcery-academy-front-end/static/js/2.d8e48c94.chunk.js"
+  },
+  {
+    "revision": "76c167b2cb872e63880d7ad57d547481",
+    "url": "/sourcery-academy-front-end/static/js/2.d8e48c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "264ad4565f756c146f75",
+    "url": "/sourcery-academy-front-end/static/js/main.7e2db476.chunk.js"
+  },
+  {
+    "revision": "18246613641c0e7b4f4e",
+    "url": "/sourcery-academy-front-end/static/js/runtime-main.1d7fe74d.js"
+  },
+  {
+    "revision": "aebac0f6d600f8ccbf7f944134157fd9",
+    "url": "/sourcery-academy-front-end/static/media/Brunch.aebac0f6.svg"
+  },
+  {
+    "revision": "8787a7ae5863d02f8359d4d3463f20ab",
+    "url": "/sourcery-academy-front-end/static/media/Burger.8787a7ae.svg"
+  },
+  {
+    "revision": "6ceb17627110a6b3d7d087f7d5ae84df",
+    "url": "/sourcery-academy-front-end/static/media/Donuts.6ceb1762.svg"
+  },
+  {
+    "revision": "cae81bd09f83bee5599ad3623067d585",
+    "url": "/sourcery-academy-front-end/static/media/Grill.cae81bd0.svg"
+  },
+  {
+    "revision": "ca2723fcc53430eb0071d53463edf4a8",
+    "url": "/sourcery-academy-front-end/static/media/Hot dogs.ca2723fc.svg"
+  },
+  {
+    "revision": "0244dda2beeb0946a8376b4d2d49c4c0",
+    "url": "/sourcery-academy-front-end/static/media/Kebab.0244dda2.svg"
+  },
+  {
+    "revision": "352af9c0f173137742c7c1ee2fa20e38",
+    "url": "/sourcery-academy-front-end/static/media/Pancakes.352af9c0.svg"
+  },
+  {
+    "revision": "75037affe89aaa6e3ec577458223a759",
+    "url": "/sourcery-academy-front-end/static/media/Pizza.75037aff.svg"
+  },
+  {
+    "revision": "ad5fa1f97b37b5a4636ab55e3099b070",
+    "url": "/sourcery-academy-front-end/static/media/Ramen.ad5fa1f9.svg"
+  },
+  {
+    "revision": "301fe70f8f0f41c236317504ec05f820",
+    "url": "/sourcery-academy-front-end/static/media/Roboto-Black.301fe70f.ttf"
+  },
+  {
+    "revision": "9ece5b48963bbc96309220952cda38aa",
+    "url": "/sourcery-academy-front-end/static/media/Roboto-Bold.9ece5b48.ttf"
+  },
+  {
+    "revision": "6090d256d88dcd7f0244eaa4a3eafbba",
+    "url": "/sourcery-academy-front-end/static/media/Roboto-Light.6090d256.ttf"
+  },
+  {
+    "revision": "b2d307df606f23cb14e6483039e2b7fa",
+    "url": "/sourcery-academy-front-end/static/media/Roboto-Medium.b2d307df.ttf"
+  },
+  {
+    "revision": "f36638c2135b71e5a623dca52b611173",
+    "url": "/sourcery-academy-front-end/static/media/Roboto-Regular.f36638c2.ttf"
+  },
+  {
+    "revision": "d45c3bd61170bebcf78b606cfa078bab",
+    "url": "/sourcery-academy-front-end/static/media/Salads.d45c3bd6.svg"
+  },
+  {
+    "revision": "7f28b8047e24b9e1907ca9e9e43539c1",
+    "url": "/sourcery-academy-front-end/static/media/Sandwich.7f28b804.svg"
+  },
+  {
+    "revision": "149c686aad1fb4c064d1a9978d89d711",
+    "url": "/sourcery-academy-front-end/static/media/Soups.149c686a.svg"
+  },
+  {
+    "revision": "40e91f04b99cb24a776020278b85cd65",
+    "url": "/sourcery-academy-front-end/static/media/Sushi.40e91f04.svg"
+  },
+  {
+    "revision": "9262ac24205ac4dd828c0a137d59c0d6",
+    "url": "/sourcery-academy-front-end/static/media/Sweets.9262ac24.svg"
+  },
+  {
+    "revision": "63f00c24b666b2f31a8602921de88241",
+    "url": "/sourcery-academy-front-end/static/media/TeamSpace-logo.63f00c24.svg"
+  },
+  {
+    "revision": "4d869411c2b4bbdc7cef944c034ac896",
+    "url": "/sourcery-academy-front-end/static/media/available-icon-active.4d869411.svg"
+  },
+  {
+    "revision": "12de87604e8e06ca55b63d14a8a488eb",
+    "url": "/sourcery-academy-front-end/static/media/available-icon.12de8760.svg"
+  },
+  {
+    "revision": "547b1d3ac863f03261387b7fdbf91de8",
+    "url": "/sourcery-academy-front-end/static/media/available-item.547b1d3a.svg"
+  },
+  {
+    "revision": "5f8c32f76e780c90f88a9b4244453fff",
+    "url": "/sourcery-academy-front-end/static/media/avatar.5f8c32f7.svg"
+  },
+  {
+    "revision": "625186e16723e455a2105cc9f42e5257",
+    "url": "/sourcery-academy-front-end/static/media/birthday-left.625186e1.svg"
+  },
+  {
+    "revision": "708e6b25ea9ea8e87cdebec610b29ea9",
+    "url": "/sourcery-academy-front-end/static/media/birthday-right.708e6b25.svg"
+  },
+  {
+    "revision": "78b1f6e2a55285058dd919150451b167",
+    "url": "/sourcery-academy-front-end/static/media/booked-item.78b1f6e2.svg"
+  },
+  {
+    "revision": "092f9db26eee49ac518e0cdd23922d14",
+    "url": "/sourcery-academy-front-end/static/media/breadcrumbs-arrow.092f9db2.svg"
+  },
+  {
+    "revision": "1a9b78ad3f8bc92d20ef961dd80c806b",
+    "url": "/sourcery-academy-front-end/static/media/burger-menu-close.1a9b78ad.svg"
+  },
+  {
+    "revision": "58f5b8bf35c2323ac10f92afbaca8021",
+    "url": "/sourcery-academy-front-end/static/media/burger-menu.58f5b8bf.svg"
+  },
+  {
+    "revision": "c00f5033ca2538d2c5355701752023e4",
+    "url": "/sourcery-academy-front-end/static/media/calendar-icon.c00f5033.svg"
+  },
+  {
+    "revision": "06f8a93637273f3b0d2f528cebc748a0",
+    "url": "/sourcery-academy-front-end/static/media/check-icon.06f8a936.svg"
+  },
+  {
+    "revision": "5ac2f923498b73d3139e82f866d38cc9",
+    "url": "/sourcery-academy-front-end/static/media/clear-icon.5ac2f923.svg"
+  },
+  {
+    "revision": "cc37d48f38bcd1e948cf9e07cfc31976",
+    "url": "/sourcery-academy-front-end/static/media/clock.cc37d48f.svg"
+  },
+  {
+    "revision": "a7a2126e6ff823b3845372f22cd70c93",
+    "url": "/sourcery-academy-front-end/static/media/close-dark-icon.a7a2126e.svg"
+  },
+  {
+    "revision": "521d195ae79d6133ea82fb7064a81e08",
+    "url": "/sourcery-academy-front-end/static/media/close-white-icon.521d195a.svg"
+  },
+  {
+    "revision": "d03c3e52db351737f6a76e92b7fe96e1",
+    "url": "/sourcery-academy-front-end/static/media/cloudy.d03c3e52.svg"
+  },
+  {
+    "revision": "ce29ce8f7fed6f98158a38c7a8dce5dd",
+    "url": "/sourcery-academy-front-end/static/media/comment-icon.ce29ce8f.svg"
+  },
+  {
+    "revision": "6e5f6ca4be569c42889e3554a6e888a5",
+    "url": "/sourcery-academy-front-end/static/media/confused-travolta.6e5f6ca4.gif"
+  },
+  {
+    "revision": "ab69bc1f0b1d315d936dbe05c21e0cac",
+    "url": "/sourcery-academy-front-end/static/media/divider.ab69bc1f.svg"
+  },
+  {
+    "revision": "ae4333b777b5f5e62eb895cc8c69fb80",
+    "url": "/sourcery-academy-front-end/static/media/ellipse.ae4333b7.svg"
+  },
+  {
+    "revision": "4a8353bc8854dbe32846db5a35e83506",
+    "url": "/sourcery-academy-front-end/static/media/error-icon.4a8353bc.svg"
+  },
+  {
+    "revision": "13c69ae3fceb0fb58f10741d03caef06",
+    "url": "/sourcery-academy-front-end/static/media/favorites-icon-active.13c69ae3.svg"
+  },
+  {
+    "revision": "644428fa3f05c1cdcf0fe00cd00e7744",
+    "url": "/sourcery-academy-front-end/static/media/favorites-icon.644428fa.svg"
+  },
+  {
+    "revision": "15318e2333e732e3dc248dac93ba589c",
+    "url": "/sourcery-academy-front-end/static/media/filled-heart.15318e23.svg"
+  },
+  {
+    "revision": "8e4637a26526102724fe18aaf0168e44",
+    "url": "/sourcery-academy-front-end/static/media/filled-star.8e4637a2.svg"
+  },
+  {
+    "revision": "0ef00f84d35cd5f3c92838cd5c768471",
+    "url": "/sourcery-academy-front-end/static/media/globe-outlined.0ef00f84.svg"
+  },
+  {
+    "revision": "25ab6edaac49bdb8966f43648c26b9c7",
+    "url": "/sourcery-academy-front-end/static/media/globe.25ab6eda.svg"
+  },
+  {
+    "revision": "bd563a1959b2f4c4a9bf4f4e2fda9af2",
+    "url": "/sourcery-academy-front-end/static/media/heart.bd563a19.svg"
+  },
+  {
+    "revision": "7f56e8e087725493f084adde45329e57",
+    "url": "/sourcery-academy-front-end/static/media/humidity.7f56e8e0.svg"
+  },
+  {
+    "revision": "67f48119e497a61ca0103c579fbf83a2",
+    "url": "/sourcery-academy-front-end/static/media/info-icon.67f48119.svg"
+  },
+  {
+    "revision": "ce180ad45a7e3564da36d027540073b9",
+    "url": "/sourcery-academy-front-end/static/media/like-icon.ce180ad4.svg"
+  },
+  {
+    "revision": "5d12e154347e1b3cfb227d2bea3053ca",
+    "url": "/sourcery-academy-front-end/static/media/like-pressed-icon.5d12e154.svg"
+  },
+  {
+    "revision": "23b8349480009bc33bda0a3452e8a547",
+    "url": "/sourcery-academy-front-end/static/media/log-out.23b83494.svg"
+  },
+  {
+    "revision": "61f9f3b1a3543115b7aeb2921cb499a4",
+    "url": "/sourcery-academy-front-end/static/media/map-pin-outlined.61f9f3b1.svg"
+  },
+  {
+    "revision": "34ecbf74dc5d30de9675551993d4c5d5",
+    "url": "/sourcery-academy-front-end/static/media/map-pin.34ecbf74.svg"
+  },
+  {
+    "revision": "5d63c65e17a3a737bc44c771cb959d62",
+    "url": "/sourcery-academy-front-end/static/media/nav-button-arrow-left.5d63c65e.svg"
+  },
+  {
+    "revision": "d1d9c3c4ee07628b5104d2db6c0bc278",
+    "url": "/sourcery-academy-front-end/static/media/nav-button-arrow-right.d1d9c3c4.svg"
+  },
+  {
+    "revision": "36ef266d98e79654566694240f49b0d9",
+    "url": "/sourcery-academy-front-end/static/media/nav-dashboard-logo.36ef266d.svg"
+  },
+  {
+    "revision": "2d65337ca1c534a26bbf4b68fdfe0496",
+    "url": "/sourcery-academy-front-end/static/media/nav-eat-out-logo.2d65337c.svg"
+  },
+  {
+    "revision": "749daeaa70181103ffaae045e06f5e1e",
+    "url": "/sourcery-academy-front-end/static/media/nav-main-logo.749daeaa.svg"
+  },
+  {
+    "revision": "de1f4df7f270be7a09a2df6c712de50f",
+    "url": "/sourcery-academy-front-end/static/media/nav-reservations-logo.de1f4df7.svg"
+  },
+  {
+    "revision": "05712080d843631e5797e36c6c76a3f3",
+    "url": "/sourcery-academy-front-end/static/media/notification.05712080.svg"
+  },
+  {
+    "revision": "1d5a379628235d1486997a0feabbbe26",
+    "url": "/sourcery-academy-front-end/static/media/outline-heart.1d5a3796.svg"
+  },
+  {
+    "revision": "0fab8ba67c5a515fd04bd7e094f1b619",
+    "url": "/sourcery-academy-front-end/static/media/phone.0fab8ba6.svg"
+  },
+  {
+    "revision": "57fbacdbb9381bd3e486c5ddf7cba02b",
+    "url": "/sourcery-academy-front-end/static/media/play-button.57fbacdb.svg"
+  },
+  {
+    "revision": "3a8ceeb1d318e45674a66aa07ed968b2",
+    "url": "/sourcery-academy-front-end/static/media/profile-picture.3a8ceeb1.jpg"
+  },
+  {
+    "revision": "7c5ad644c7b1ec8b5361c60aaffa8cd2",
+    "url": "/sourcery-academy-front-end/static/media/rainy.7c5ad644.svg"
+  },
+  {
+    "revision": "a8fecc0b68ebabc7eeaa7f3cd7a59542",
+    "url": "/sourcery-academy-front-end/static/media/reservations-book.a8fecc0b.svg"
+  },
+  {
+    "revision": "1dc0af892f404b0a4eeab046fcbf141f",
+    "url": "/sourcery-academy-front-end/static/media/reservations-devices.1dc0af89.svg"
+  },
+  {
+    "revision": "fe7f072094d077507f0e3f88d95f4df6",
+    "url": "/sourcery-academy-front-end/static/media/reservations-filters-btn-clear.fe7f0720.svg"
+  },
+  {
+    "revision": "ce95dfbe5179015e9331ced716733afb",
+    "url": "/sourcery-academy-front-end/static/media/reservations-meeting-room.ce95dfbe.svg"
+  },
+  {
+    "revision": "77b4a937ef2cdd68a87406cc6379d088",
+    "url": "/sourcery-academy-front-end/static/media/search-date-left.77b4a937.svg"
+  },
+  {
+    "revision": "4f46ca454686929ceddabb8c3fee76fe",
+    "url": "/sourcery-academy-front-end/static/media/search-date-right.4f46ca45.svg"
+  },
+  {
+    "revision": "a1d558f51a4cbb12c5449c823a49940a",
+    "url": "/sourcery-academy-front-end/static/media/search-icon-dark.a1d558f5.svg"
+  },
+  {
+    "revision": "6f646a6faaf4392679b31dac63ac5ba7",
+    "url": "/sourcery-academy-front-end/static/media/search.6f646a6f.svg"
+  },
+  {
+    "revision": "33e8dc316682fc78691f97f182875f4d",
+    "url": "/sourcery-academy-front-end/static/media/settings.33e8dc31.svg"
+  },
+  {
+    "revision": "5b13d485f2692c72e7ef439170857f64",
+    "url": "/sourcery-academy-front-end/static/media/slider-icon.5b13d485.svg"
+  },
+  {
+    "revision": "193d1f0e3b57440875dbc326fababd24",
+    "url": "/sourcery-academy-front-end/static/media/slider-open-icon.193d1f0e.svg"
+  },
+  {
+    "revision": "e95bbe2c47099c88170197bd11b0feab",
+    "url": "/sourcery-academy-front-end/static/media/snow.e95bbe2c.svg"
+  },
+  {
+    "revision": "c41ca5d606636d82622e60f4b8bcfca9",
+    "url": "/sourcery-academy-front-end/static/media/star-outline.c41ca5d6.svg"
+  },
+  {
+    "revision": "973096e7152e8487be8183d355b3029e",
+    "url": "/sourcery-academy-front-end/static/media/success-icon.973096e7.svg"
+  },
+  {
+    "revision": "0e8eb8e3a775657281bff7d72d7087bf",
+    "url": "/sourcery-academy-front-end/static/media/sunny.0e8eb8e3.svg"
+  },
+  {
+    "revision": "00169510bfef2081f5545cdec711f13b",
+    "url": "/sourcery-academy-front-end/static/media/thunderstorm.00169510.svg"
+  },
+  {
+    "revision": "98264bb8fd0e86b5b5e464f05fa47ca4",
+    "url": "/sourcery-academy-front-end/static/media/wind.98264bb8.svg"
+  },
+  {
+    "revision": "6ecac5a3cc4acb8cb54b3366da75b07f",
+    "url": "/sourcery-academy-front-end/static/media/wish-icon.6ecac5a3.svg"
+  },
+  {
+    "revision": "a80754e19bde4360967fe2ac66551f0d",
+    "url": "/sourcery-academy-front-end/static/media/wish-pressed-icon.a80754e1.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sourcery-academy-front-end/precache-manifest.fb2b7de6d0702cf7ae0f5c9db678e2b7.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sourcery-academy-front-end/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
